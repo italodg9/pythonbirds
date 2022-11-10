07b4f9ebb991438262f626da31df4fda2ca077f3
Italodg9 <italodg9@outlook.com>
2022-11-09 22:10:35 -0300

    arrumando o codigo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,11 @@
 class Pessoa:
     def cumprimentar(self):
-        return f'Olá'
+        return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
